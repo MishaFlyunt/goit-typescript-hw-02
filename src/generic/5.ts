76a9018b05a44f36befe3a6d8f609882fa57248b
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-10-14 12:32:09 +0300

    add

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,14 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<T, V> {
+  key: T;
+  value: V;
 }
+
+let a: KeyValuePair<number, string> = {
+  key: 2,
+  value: "4",
+};
 
 export {};
